5722efaba21af251901f0d4575792d104c828f30
silence <m18679658563@163.com>
2020-08-27 21:03:33 +0800

    test

--- src/main/java/com/springboot/SpringbootApplication.java
+++ src/main/java/com/springboot/SpringbootApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-
 @EnableCaching//开启缓存
 @SpringBootApplication
 public class SpringbootApplication {
